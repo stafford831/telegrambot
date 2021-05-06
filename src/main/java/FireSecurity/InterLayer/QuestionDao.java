beb0fc98a605bb6f47eb2c12621800e7c1bbf46e
stafford831 <79166438+stafford831@users.noreply.github.com>
2021-05-06 23:26:29 +0400

    Update QuestionDao.java

--- src/main/java/FireSecurity/InterLayer/QuestionDao.java
+++ src/main/java/FireSecurity/InterLayer/QuestionDao.java
@@ -4,9 +4,6 @@ import FireSecurity.Models.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * погуглить про DAO
- */
 @Repository
 public interface QuestionDao extends JpaRepository<Question, Long> {
 }
