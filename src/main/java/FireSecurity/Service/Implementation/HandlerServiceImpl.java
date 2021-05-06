18572ebfca4c9889fed4537877d347c833a36ff7
stafford831 <79166438+stafford831@users.noreply.github.com>
2021-05-06 23:30:12 +0400

    Update HandlerServiceImpl.java

--- src/main/java/FireSecurity/Service/Implementation/HandlerServiceImpl.java
+++ src/main/java/FireSecurity/Service/Implementation/HandlerServiceImpl.java
@@ -20,19 +20,12 @@ import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
-/**
- * реализация handlerService
- *
- */
 public class HandlerServiceImpl implements HandlerService {
 
     private final Map<String, BaseHandler> handlers;
 
 
     @Getter
-    /**
-     * формирование сообщение справки по handlerом в мапе
-     */
     private final UpdateHandler defaultHandler = new UpdateHandler() {
         @Override
         public List<SendMessage> handle(Update update) {
@@ -47,11 +40,6 @@ public class HandlerServiceImpl implements HandlerService {
         }
     };
 
-    /**
-     * достаем тип обработчика из текста или колбека и возвращаем обработчик по этому ключу из мапы
-     * @param update
-     * @return обработчик по этому ключу из мапы
-     */
     @Override
     public UpdateHandler getHandler(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
@@ -66,42 +54,20 @@ public class HandlerServiceImpl implements HandlerService {
         return null;
     }
 
-    /**
-     *
-     * @param key принимает ключ под которым handler сохранен в мапе,
-     *            ключ это имя объекта, которое передается внутри аннотации component
-     * @return обработчик сообщения
-     */
     @Override
     public UpdateHandler getHandler(String key) {
         return handlers.get(key);
     }
 
-    /**
-     * обрабатываем строку введенную пользователем с целью получения ключа в мапе
-     * @param s строка введенная пользователем
-     * @return слово до первого пробела, которое является командой
-     */
     private String extractHandlerNameFromText(String s) {
         return s.split(" ")[0];
     }
 
-    /**
-     * получаем имя обработчика/ключ к мапе из содержимого колбека,
-     * содержимое это джейсон, в одном из полей ключ
-     * @param s колбек
-     * @return имя компонента/обработчика
-     * @throws ParseException
-     */
     private String extractHandlerNameFromCallback(String s) throws ParseException {
         JSONObject data = (JSONObject) new JSONParser().parse(s);
         return (String)data.get("handlerName");
     }
 
-    /**
-     * формируем строку со справкой, склеивая описание из обработчиков сообщения пользователя
-     * @return строка справки
-     */
     private String getHandlersDescription() {
         StringBuilder builder = new StringBuilder();
         builder.append("Список доступных команд: \n");
