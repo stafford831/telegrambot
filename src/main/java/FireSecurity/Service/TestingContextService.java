e8c6d2ef02d13bad6db55bb1253a564fa5af68b6
stafford831 <79166438+stafford831@users.noreply.github.com>
2021-05-06 23:33:03 +0400

    Update TestingContextService.java

--- src/main/java/FireSecurity/Service/TestingContextService.java
+++ src/main/java/FireSecurity/Service/TestingContextService.java
@@ -5,9 +5,6 @@ import FireSecurity.Models.Question;
 
 import java.util.List;
 
-/**
- * интерфейс для работы с текущим контекстом тестирования для конкретного пользователя, определяемого по телеграмм id
- */
 public interface TestingContextService {
 
     List<Question> getContext(Long tId);
