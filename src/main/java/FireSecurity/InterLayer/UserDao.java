1cac489e2645efcd6e5f90d87aa7119c51cecce5
stafford831 <79166438+stafford831@users.noreply.github.com>
2021-05-06 23:27:13 +0400

    Update UserDao.java

--- src/main/java/FireSecurity/InterLayer/UserDao.java
+++ src/main/java/FireSecurity/InterLayer/UserDao.java
@@ -3,10 +3,6 @@ package FireSecurity.InterLayer;
 import FireSecurity.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * погуглить про DAO, DAO factory
- *
- */
 public interface UserDao extends JpaRepository<User, Long> {
 
     User findByTelegramId(Long tId);
